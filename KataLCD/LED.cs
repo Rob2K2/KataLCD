cce9a9acfddca9d4ba9f2d08ab74651967397589
Roberto Merino <rmerino@bco.com.bo>
2019-10-16 18:13:40 -0400

    Primer commit con ya algunos test hechos y alguno borrado, se comentó el bloque donde se hizo la 1era version del método NumberToLCD

---
+++ KataLCD/LED.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLCD
+{
+    public class LED
+    {
+        //Tipos de led
+        private static readonly string _NONE = "   ";
+        private static readonly string _LEFT = "  |";
+        private static readonly string _MIDL = " _ ";
+        private static readonly string _MDLT = " _|";
+        private static readonly string _MDRT = "|_ ";
+        private static readonly string _FULL = "|_|";
+        private static readonly string _BOTH = "| |";
+
+        private static readonly Dictionary<char, string[]> Digitos = new Dictionary<char, string[]>
+        {
+            {'0', new string[] { _MIDL, _BOTH, _FULL }},
+            {'1', new string[] { _NONE, _LEFT, _LEFT }},
+            {'2', new string[] { _MIDL, _MDLT, _MDRT }},
+            {'3', new string[] { _MIDL, _MDLT, _MDLT }},
+            {'4', new string[] { _NONE, _FULL, _LEFT }},
+            {'5', new string[] { _MIDL, _MDRT, _MDLT }},
+            {'6', new string[] { _MIDL, _MDRT, _FULL }},
+            {'7', new string[] { _MIDL, _LEFT, _LEFT }},
+            {'8', new string[] { _MIDL, _FULL, _MDLT }},
+            {'9', new string[] { _MIDL, _BOTH, _FULL }}
+        };
+
+
+        //1era version de los cambios para las primeras pruebas
+        //public string[] NumberToLCD(int number)
+        //{
+        //    if (number == 1)
+        //    {
+        //        return new string[] { _NONE, _LEFT, _LEFT };
+        //    }
+
+        //    if (number == 2)
+        //    {
+        //        return new string[] { _MIDL, _MDLT, _MDRT };
+        //    }
+
+        //    if (number == 3)
+        //    {
+        //        return new string[] { _MIDL, _MDLT, _MDLT };
+        //    }
+
+        //    return null;
+        //}
+
+        public string[] NumberToLCD(int number)
+        {
+            var n = Convert.ToChar(number.ToString());
+
+            return Digitos[n];
+        }
+    }
+}
---
+++ KataTest/LEDTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using KataLCD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTest
+{
+    [TestClass]
+    public class LEDTest
+    {
+        readonly LED led = new LED();
+
+        [TestMethod]
+        public void ShowNumberOneLeds()
+        {
+            string[] one = new string[] {  "   ", "  |", "  |" };
+
+            CollectionAssert.AreEqual(one, led.NumberToLCD(1));
+        }
+
+        [TestMethod]
+        public void ShowNumberTwoLeds()
+        {
+            string[] two = new string[] { " _ ", " _|", "|_ " };
+
+            CollectionAssert.AreEqual(two, led.NumberToLCD(2));
+        }
+
+        [TestMethod]
+        public void ShowNumberThreeLeds()
+        {
+            string[] three = new string[] { " _ ", " _|", " _|" };
+
+            CollectionAssert.AreEqual(three, led.NumberToLCD(3));
+        }
+    }
+}
